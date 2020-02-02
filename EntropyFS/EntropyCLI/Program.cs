79541ebc7e6f27b4c762ea8b266cb0239b321c5a
Connor Greene <connor@connorgreene.net>
2020-02-01 23:28:18 -0500

    Completed preliminary EntityFS Compressor

---
+++ EntropyFS/EntropyCLI/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace EntropyCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EntropyCLI!\n");
+
+            /*
+            Console.WriteLine("Running benchmarks to determine which block size is ideal for you...\n");
+            Benchmark(MD5.Create(), "MD5", 1, "+ 1600%");
+            Benchmark(MD5.Create(), "MD5", 2, "+ 800%");
+            Benchmark(MD5.Create(), "MD5", 3, "+ 533%");
+            Benchmark(MD5.Create(), "MD5", 4, "+ 400%");
+            Benchmark(MD5.Create(), "MD5", 5, "+ 320%");
+            Benchmark(MD5.Create(), "MD5", 6, "+ 266%");
+            Benchmark(MD5.Create(), "MD5", 7, "+ 228%");
+            Benchmark(MD5.Create(), "MD5", 8, "+ 200%");
+            */
+
+            var inputFile = "c:\\users\\connor\\documents\\github\\entropyfs\\entropyfs\\entropycli\\input.txt";
+            var outputFile = "c:\\users\\connor\\documents\\github\\entropyfs\\entropyfs\\entropycli\\output.efs";
+
+            EntropyFS.Compressor.CompressFile(MD5.Create(), 3, inputFile, outputFile);
+        }
+
+
+        private static void Benchmark(HashAlgorithm algo, string sAlgorithm, int blockSize, string sRatio)
+        {
+            byte[] block = new byte[blockSize];
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int testCycles = 1000000;
+
+            for (int i = 0; i < testCycles; i++)
+            {
+                algo.ComputeHash(block);
+            }
+            stopwatch.Stop();
+
+            long hashesPerSecond = testCycles * 1000 / stopwatch.ElapsedMilliseconds;
+
+            // compute the storage costs
+            var maxCyclesPerBlock = BigInteger.Pow(2, blockSize * 8);
+            var avgCyclesPerBlock = BigInteger.Divide(maxCyclesPerBlock, 2);
+
+            var avgSecondsPerBlock = BigInteger.Divide(avgCyclesPerBlock, hashesPerSecond);
+
+            try
+            {
+                var timePerBlock = TimeSpan.FromSeconds((double)avgSecondsPerBlock);
+                Console.WriteLine($"{sAlgorithm} - {blockSize} byte blocks. {hashesPerSecond.ToString("N0")} hashes per second. Avg block compute time: {timePerBlock}. Size Change: {sRatio}");
+            }
+            catch (OverflowException)
+            {
+                // block size is too large for this system
+                return;
+            }
+        }
+    }
+}
---
+++ EntropyFS/EntropyFS/Compressor.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace EntropyFS
+{
+    public class Compressor
+    {
+        public static void CompressFile(HashAlgorithm algo, int blockSize, string inputFilePath, string outputFilePath)
+        {
+            var inputBytes = File.ReadAllBytes(inputFilePath);
+
+            try
+            {
+                File.Delete(outputFilePath);
+            }
+            catch { }
+
+            var outputFile = File.OpenWrite(outputFilePath);
+
+            // read through the input file
+            int idx = 0;
+            while (idx < inputBytes.Length)
+            {
+                var block = new byte[blockSize];
+
+                // load the block for hashing
+                for (int i = 0; i < blockSize; i++)
+                {
+                    block[i] = inputBytes[idx++];
+
+                    if (idx >= inputBytes.Length)
+                    {
+                        break;
+                    }
+                }
+
+                var targetHash = algo.ComputeHash(block);
+
+                // hash it out
+                byte collisionIdx = 0;
+                var workingBlock = new byte[blockSize];
+
+                var iterations = BigInteger.Pow(256, blockSize);
+                for (BigInteger i = 0; i < iterations; i++)
+                {
+                    var hash = algo.ComputeHash(workingBlock);
+                    if (targetHash.SequenceEqual(hash))
+                    {
+                        if (block.SequenceEqual(workingBlock))
+                        {
+                            outputFile.Write(hash, 0, hash.Length);
+
+                            // write collision index to file (max 256 for the byte type, hopefully that's not a problem)
+                            outputFile.WriteByte(collisionIdx);
+
+                            break;
+                        }
+                        else
+                        {
+                            collisionIdx++;
+                        }
+                    }
+
+                    IncrementBlock(ref workingBlock);
+                }
+            }
+
+            outputFile.Close();
+        }
+
+        private static void IncrementBlock(ref byte[] block, int idx = 0)
+        {
+            if (idx >= block.Length)
+            {
+                return;
+            }
+
+            block[idx]++;
+
+            // recursively perform the carryover
+            if (block[idx] == 0)
+            {
+                IncrementBlock(ref block, idx + 1);
+            }
+        }
+    }
+}
